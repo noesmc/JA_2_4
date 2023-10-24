19bd1925916135ef46e6b39b52b32151e5e1ef09
noesmc <nikita_sinykovich@mail.ru>
2023-10-24 08:41:40 +0300

    JA_2_4

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,49 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] channel =
-                { "Exit", "BT", "ONT", "BT5", "NTV",
-                        "TNT", "Victory", "MTV", "Nickelodeon", "Disney"
-                };
-        int amount = channel.length;
-        System.out.println("Program is: ");
+//        String[] channel =
+//                { "Exit", "BT", "ONT", "BT5", "NTV",
+//                        "TNT", "Victory", "MTV", "Nickelodeon", "Disney"
+//                };
+////        int amount = channel.length;
+////        System.out.println("Program is: ");
+////
+//////        for (int i = 1; i < amount; i++) {
+//////            System.out.println(i + " - " + channel[i]);
+//////            if (i == amount - 1) {
+//////                System.out.println("0 - Exit");
+//////            }
+//////        }
 
-        for (int i = 1; i < amount; i++) {
-            System.out.println(i + " - " + channel[i]);
-            if (i == amount - 1) {
-                System.out.println("0 - Exit");
-            }
+        String[] channels = {"1 - BT", "2 - ONT", "3 - BT5", "4 - NTV",
+                "5 - TNT", "6 - Victory", "7 - MTV", "8 - Nickelodeon", "9 - Disney"};
+        for (String channel : channels) {
+            System.out.println(channel);
         }
+        System.out.println("0  - Exit");
 
         Scanner scanner = new Scanner(System.in);
-        int x;
+        int x = 0;
+        boolean exit = false;
 
-        while (true) {
+        while (!exit) {
             try {
                 System.out.print("Enter the channel number: ");
                 x = scanner.nextInt();
-                System.out.println("Entered channel is: " + channel[x]);
+
                 if (x == 0) {
-                    System.out.println("You finally enter " + channel[x]);
-                    break;
+                    System.out.println("You enter \"Exit\"");
+                    exit = true;
+                } else {
+                    System.out.println(channels[x - 1]);
                 }
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                {
+            } catch (Exception e) {
+                if (e instanceof ArrayIndexOutOfBoundsException) {
                     System.out.println("Channel is unavailable!");
+                }   else {
+                    System.out.print("Wrong input!");
                 }
             }
         }
